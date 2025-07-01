52a448cf13c1ef797c2c60603d48f6516cf63f88
Sgiane11a <steyci.cordova@tecsup.edu.pe>
2025-06-30 22:51:07 -0500

    Galeria Integrada

--- src/pages/Recursos.jsx
+++ src/pages/Recursos.jsx
@@ -1,17 +1,26 @@
 import Carrusel from "../components/Recursos/Carrusel"
+import Gallery from "../components/Recursos/Gallery"
 
 export const Recursos = () => {
     return (
-        
+
         <main className="navbar-expand-lg bg-body-tertiary">
-            <section className="container">   
             <h1 className="text-center">Recursos</h1>
             <p className="text-center">( Aquí tienes algunos recursos )</p>
-            <h1>EJEMPLO DE CARRUSEL</h1>
-            <div>
-                <Carrusel />
-            </div>
+            <section className="container my-5">
+                <h1>EJEMPLO DE CARRUSEL</h1>
+                <div className="container my-5">
+                    <Carrusel />
+                </div>
             </section>
+
+            <section className="container my-5">
+                <h1>EJEMPLO DE UN GALERIA</h1>
+                <div className="container my-5">
+                    <Gallery />
+                </div>
+            </section>
+
         </main>
     )
 }
